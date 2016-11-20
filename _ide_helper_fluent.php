074294e202acfb763e1d2acec8598da63d858a9f
Chen Ji <fzls.zju@gmail.com>
2016-11-20 21:03:45 +0800

    添加Schema的ide提示：通过删除文件头的use ...Schema

---
+++ _ide_helper_fluent.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 风之凌殇
+ * Date: 6/25/2016
+ * Time: 10:22 AM
+ */
+namespace Illuminate\Support;
+
+/**
+ * @method Fluent first()
+ * @method Fluent after($column)
+ * @method Fluent change()
+ * @method Fluent nullable()
+ * @method Fluent unsigned()
+ * @method Fluent unique()
+ * @method Fluent index()
+ * @method Fluent primary()
+ * @method Fluent default($value)
+ * @method Fluent onUpdate($value)
+ * @method Fluent onDelete($value)
+ * @method Fluent references($value)
+ * @method Fluent on($value)
+ */
+class Fluent {}
