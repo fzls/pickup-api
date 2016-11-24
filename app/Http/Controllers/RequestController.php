518213680a962c2009d80a7699cdee249bfb9830
Chen Ji <fzls.zju@gmail.com>
2016-11-24 21:34:16 +0800

    准备工作完成

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PickupApi\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * 通过redis实现数据持久化
+ *
+ * Class RequestController
+ * @package PickupApi\Http\Controllers
+ */
+class RequestController extends Controller
+{
+    public function getRequestList(){
+
+    }
+
+    public function addNewRequestToList(){
+
+    }
+
+    public function driverAcceptRequest(){
+
+    }
+
+    public function passengerCancelRequest(){
+
+    }
+}
