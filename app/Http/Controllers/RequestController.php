cb3a7eede7df3d58386d472ac73b60456a65b7af
Chen Ji <fzls.zju@gmail.com>
2016-11-24 22:04:06 +0800

    添加用户请求表代替redis实现

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -10,21 +10,45 @@ use Illuminate\Http\Request;
  * Class RequestController
  * @package PickupApi\Http\Controllers
  */
-class RequestController extends Controller
-{
-    public function getRequestList(){
+class RequestController extends Controller {
+    public $redis;
+    public $redis_key;
+
+    public function __construct(Request $request) {
+        parent::__construct($request);
+        $this->redis     =\Redis::connection();
+        $this->redis_key = 'request_list';
+    }
+
+
+    public function getRequestList() {
+        $this->redis->get($this->redis_key);
+    }
+
+    public function addNewRequestToList() {
+        $this->validate(
+            $this->request,
+            [
+                'start_name'=>'string',
+                'start_latitude'=>'required|numeric|min:-180|max:180',
+                'start_longitude'=>'required|numeric|min:-90|max:90',
+                'end_name'=>'string',
+                'end_latitude'=>'required|numeric|min:-180|max:180',
+                'end_longitude'=>'required|numeric|min:-90|max:90',
+                'expected_vehicle_type'=>'exists',
+                'activity'=>
+            ]
+        )
+        $this->redis->lpush($this->redis_key,[
+
+        ]);
+    }
+
+    public function driverAcceptRequest() {
 
     }
 
-    public function addNewRequestToList(){
-
-    }
-
-    public function driverAcceptRequest(){
-
-    }
-
-    public function passengerCancelRequest(){
+    public function passengerCancelRequest() {
 
     }
 }
---
+++ app/Models/Request.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chen
+ * Date: 2016-11-24
+ * Time: 22:00
+ */
+
+namespace PickupApi\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Request extends Model {
+    protected $table      = 'requests';
+
+    public    $timestamps = true;
+
+    protected $fillable
+                          = [
+            'start_name',
+            'start_latitude',
+            'start_longitude',
+            'end_name',
+            'end_latitude',
+            'end_longitude',
+            'expected_vehicle_type',
+            'activity',
+            'phone_number',
+            'estimated_cost',
+            'reserved_at'
+        ];
+
+    protected $guarded    = [];
+}
