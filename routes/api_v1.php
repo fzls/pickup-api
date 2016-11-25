84977af0dd5c7a74915eea439ccbcadf9f599b0a
Chen Ji <fzls.zju@gmail.com>
2016-11-25 11:54:53 +0800

    添加必要的接口url的定义

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -60,3 +60,97 @@ Route::group([/*车单接口*/], function () {
     Route::put('/requests', 'RequestController@driverAcceptRequest');
     Route::delete('/requests', 'RequestController@passengerCancelRequest');
 });
+
+/*Hack: 从这里开始*/
+Route::group([/*历史行程接口*/],function (){
+    Route::get('/history','');
+    Route::post('/history','');
+    Route::get('/history/{history}','');
+    Route::delete('/history/{history}','');
+});
+
+Route::group([/*历史行程快照接口*/], function (){
+    Route::get('/history_snapshots', '');
+    Route::post('/history_snapshots','');
+});
+
+Route::group([/*当前位置接口*/], function (){
+    Route::get('/current_location', '');
+    Route::post('/current_location', '');
+    Route::delete('/current_location', '');
+});
+
+Route::group([/*余额相关接口*/], function (){
+    Route::get('/money', '');
+    Route::post('/money', '');
+    Route::delete('/money', '');
+    Route::post('/transfer/{to}', '');
+});
+
+Route::group([/*订单相关接口*/], function (){
+    Route::get('/orders/recharges', '');
+    Route::get('/orders/withdraws', '');
+    Route::get('/orders/payments', '');
+    Route::get('/orders/revenues', '');
+});
+
+Route::group([/*礼物相关接口*/], function (){
+    Route::get('/gift-categories', '');
+    Route::post('/gift-categories', '');
+    Route::get('/gift-categories/{gift}', '');
+    Route::put('/gift-categories/{gift}', '');
+    Route::delete('/gift-categories/{gift}', '');
+    Route::get('/gifts', '');
+    Route::post('/gifts', '');
+});
+
+Route::group([/*评价与投诉接口*/], function (){
+    Route::post('/rater/{to}', '');
+    Route::post('/tousu/{to}', '');
+});
+
+Route::group([/*排行榜相关接口*/], function (){
+    Route::get('/rankings', '');
+    Route::get('/rankings/{type}', '');
+    /*获取某一种类型的排行榜{highest_rated_drivers, most_attractive_drivers, most_rated_passengers}
+    对象类别分三个，highest_rated_driver_rankings, most_attractive_driver_rankings, most_rated_passenger_rankings，
+    时间段类别分过去一天，一周，一月，以及总排行，通过选择时间周期调整，默认显示一周的结果
+*/
+});
+
+Route::group([/*聊天接口*/], function (){
+    Route::get('/chats', '');
+    Route::get('/chats/{pal}', '');
+    Route::post('/chats/{pal}', '');
+    Route::delete('/chats/{pal}', '');
+    Route::get('/chats/{pal}/{chat}', '');
+    Route::put('/chats/{pal}/{chat}', '');
+    Route::delete('/chats/{pal}/{chat}', '');
+});
+
+Route::group([/*通知相关接口*/],function (){
+    Route::get('/notifications', '');
+    Route::post('/notifications', '');
+    Route::put('/notifications', '');
+    Route::get('/notifications/read', '');
+    Route::get('/notifications/unread', '');
+    Route::get('/notifications/{notification}', '');
+    Route::put('/notifications/{notification}', '');
+    Route::delete('/notifications/{notification}', '');
+});
+
+Route::group([/*签到相关接口*/], function (){
+    Route::get('/checkin', '');
+    Route::post('/checkin', '');
+});
+
+Route::group([/*搜索相关接口*/], function (){
+    Route::get('/search', '');
+});
+
+Route::group([/*其他接口*/],function (){
+    Route::get('/about', '');
+    Route::get('/help', '');
+    Route::get('/contact', '');
+    Route::get('/version', '');
+});
